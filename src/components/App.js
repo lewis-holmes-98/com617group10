5b7de392df4b4c6e402853c77a0ec7fc26d3eba4
4holml09 <4holml09@solent.ac.uk>
2022-03-09 19:50:15 +0000

    got bloody tailwind working

--- src/components/App.js
+++ src/components/App.js
@@ -4,9 +4,9 @@ import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
 class App extends React.Component {
     render() {
         return (
-            <div className="App">
-                <h1 className="text-3xl font-bold underline">TEST</h1>
-            </div>
+            
+                <div className="text-xl mb-2 text-blue-700">TEST</div>
+            
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import './styles/index.css';
+import './styles/main.css';
 
 
-ReactDOM.render(<React.StrictMode>
-    <App/>
-</React.StrictMode>,
-    document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));

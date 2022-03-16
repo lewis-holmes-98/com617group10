1dccafe032b286dd418b2b955312cd83efea1218
4holml09 <4holml09@solent.ac.uk>
2022-03-16 16:46:29 +0000

    added code to render header

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
+import Header from './header'
 
 class App extends React.Component {
     render() {
         return (
             
-                <div className="text-xl mb-2 text-blue-700">TEST</div>
+                <Header />
             
         )
     }
